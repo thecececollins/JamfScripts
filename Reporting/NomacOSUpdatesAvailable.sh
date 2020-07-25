e7fb3598a2a60858826917410f575eff345ba211
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2020-07-24 22:11:56 -0500

    Create NomacOSUpdatesAvailable.sh

---
+++ Reporting/NomacOSUpdatesAvailable.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/osascript -e 'tell application "Self Service" to display dialog "Great news, your computer is fully up-to-date so there are no macOS updates available at this time. You rock!" buttons {"Fantastic!"} with icon caution giving up after 120'

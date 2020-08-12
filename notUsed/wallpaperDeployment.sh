bcb97076ba985fa1a5b2b5e1122e24592b176c56
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2020-08-12 09:58:20 -0500

    Update wallpaperDeployment.sh

--- notUsed/wallpaperDeployment.sh
+++ notUsed/wallpaperDeployment.sh
@@ -2,7 +2,7 @@
 # $3 is the logged in user - default for most policies.  
 sudo -u $3 /usr/bin/osascript <<ENDofOSAscript
 tell Application "Finder"
-set the desktop picture to {"Library:Desktop Pictures:narrativescience.jpg"} as alias
+set the desktop picture to {"Library:Desktop Pictures:[name of file].jpg"} as alias
 end tell
 ENDofOSAscript
 exit 0

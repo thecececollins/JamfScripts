83a9a9a0169decf990b088600ecc110a971c4faa
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2020-08-12 10:00:30 -0500

    Update computerInformation.sh

--- Triaging/computerInformation.sh
+++ Triaging/computerInformation.sh
@@ -229,7 +229,7 @@ $timeServer
 ----------------------------------------------
 
 
-Feel free to take a screenshot of this information and attach it to your Freshservice IT ticket."
+Feel free to take a screenshot of this information and attach it to your IT ticket."
 
 
 
@@ -254,7 +254,7 @@ clickedButton=$( /usr/bin/osascript -e "$runCommand" )
 	
 	# email computer information to it
 	#currentUser=$( stat -f "%Su" /dev/console )
-	#sudo -u "$currentUser" /usr/bin/open "mailto:officeit@narrativescience.com?subject=Computer Information ($serialNumber)&body=$displayInfo"
+	#sudo -u "$currentUser" /usr/bin/open "mailto:[support email]?subject=Computer Information ($serialNumber)&body=$displayInfo"
 	
 	#if [ $? = 0 ]; then
 		#/usr/bin/osascript -e 'display dialog "Remote support enabled." with title "Computer Information" with icon file posix file "/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns" buttons {"OK"} default button {"OK"}' &

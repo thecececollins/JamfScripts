ce1aa655e95fc9c0a9ad977bc596597dd823866e
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2020-08-12 09:42:33 -0500

    Update oncallSetup.sh

--- Environments/oncallSetup.sh
+++ Environments/oncallSetup.sh
@@ -6,9 +6,6 @@
 # 			- Builds on top of the Base Setup
 # 			- Installs Pants
 # 			- Installs PM2
-# 			- Adds to the NS profile:
-# 				- Pants Env Vars
-# 				- LEXIO_ROOT Env Var
 #
 #
 # Exit Code Descriptions:
@@ -17,8 +14,8 @@
 # 			exit 3 - JSS is not Accessible
 #			exit 5 - Unknown Error
 #
-# Written by: Collins | IT Lead | Narrative Science
-# Edited by: Collins | IT Lead | Narrative Science
+# Written by: Collins
+# Edited by: Collins
 #
 # Created On: October 17th, 2016
 # Updated On: October 17th, 2016
@@ -31,7 +28,7 @@
 
 # Notification template
 JH=/Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper
-TITLE="Narrative Science"
+TITLE="Company Name"
 ICON="/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/AlertNoteIcon.icns"
 ADESC="Natural"
 

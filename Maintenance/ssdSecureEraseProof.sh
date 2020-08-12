b49aa9c5913e8bc89e8d86578ac63ca12d5d50b9
Cece Collins <7505271+thecececollins@users.noreply.github.com>
2020-08-12 09:46:48 -0500

    Update ssdSecureEraseProof.sh

--- Maintenance/ssdSecureEraseProof.sh
+++ Maintenance/ssdSecureEraseProof.sh
@@ -2,7 +2,7 @@
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # This script will accomplish the following:
-# 			- Collect the name of the Mac
+# 		- Collect the name of the Mac
 #			- Collect the serial number of the Mac
 #			- Collect the serial number of the SSD
 #			- Bypass all encryption and credentials needed or use Admin credentials
@@ -14,8 +14,8 @@
 # In order for this script to work effectively you will need to run the script from one computer
 # with the target computer booted to Target Disk Mode and hardwired to the computer running the script.
 #
-# Written by: CCollins | IT Manager | 
-# Edited by: CCollins | IT Manager |  
+# Written by: CCollins
+# Edited by: CCollins
 #
 # Created On: September 26th, 2019
 # Updated On: 
@@ -63,7 +63,7 @@ Computer Serial Number: $serialNumber
 Date and Time of Completion: $timeInfo
 ----------------------------------------------
 
-This task has been completed and recorded for security purposes. These results will be stored in the Narrative Science AWS S3 Bucket."
+This task has been completed and recorded for security purposes. These results will be stored in the [Company Name] AWS S3 Bucket."
 
 # Display output
 runCommand="button returned of (display dialog \"$displayInfo\" with title \"Security Report\" with icon file posix file \"/System/Library/CoreServices/Finder.app/Contents/Resources/Finder.icns\" buttons {\"Super!\"} default button {\"Super!\"})"
